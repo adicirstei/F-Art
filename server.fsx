c10963e8650e329dd256345fb50e9a955acf6630
Adrian <adicirstei@gmail.com>
2016-06-13 17:52:36 +0300

    write png

--- server.fsx
+++ server.fsx
@@ -6,7 +6,20 @@ open System.Drawing
 open System.Drawing.Imaging
 
 
+let draw (text:string) =
+
+  let redBr = new SolidBrush(Color.Red)
+  let whiteBr = new SolidBrush(Color.Red)
+  let i = new Bitmap(600,400)
+  let g = Graphics.FromImage i
+  let f = new Font("Courier", float32 <| 24.0)
+  g.FillRectangle(whiteBr, float32 <| 0.0, float32 <| 0.0, float32 <| 600.0, float32 <| 400.0 )
+  g.DrawString(text, f, redBr, float32 <| 10.0, float32 <| 40.0)
+  g.Flush()
+  let ms = new System.IO.MemoryStream()
+  i.Save(ms, ImageFormat.Png)
+  ms.ToArray()
 
 
 
-startWebServer defaultConfig (Successful.OK "Hello World!")
+startWebServer defaultConfig (Successful.ok <| draw "bibi")
