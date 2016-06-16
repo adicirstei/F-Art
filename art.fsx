bb88e107584c97660afce7657d9d5b5665ca133c
Adrian <adicirstei@gmail.com>
2016-06-16 12:38:10 +0300

    implemented some drawing

--- art.fsx
+++ art.fsx
@@ -1,3 +1,6 @@
+#r "packages/Fsharp.Data/lib/net40/Fsharp.Data.dll"
+
+open FSharp.Data
 open System.Drawing
 
 type Particle = {
@@ -11,11 +14,26 @@ type Particle = {
 //  prev : (Float,Float)
 }
 
+type Palettes = FSharp.Data.JsonProvider<"data/palettes.json">
+type JsonPalette = Palettes.Root
+type Palette = Color array
 
 
+let strToColor (str:string) : Color =
+  ColorTranslator.FromHtml ("#" + str)
+
+let toPalette (v:Palettes.NumbersOrStrings) : Palette =
+  v.Strings
+  |> Array.map strToColor 
+
+
+let palettes =
+  Palettes.GetSamples()
+  |> Array.map (fun (i:JsonPalette) -> i.Colors |> toPalette)   
 
 
 let draw (rand:System.Random) =
+  let pal = palettes.[rand.Next palettes.Length]
   let text = rand.Next() |> sprintf "~# %d #~"
   let redBr = new SolidBrush(Color.Red)
   let whiteBr = new SolidBrush(Color.White)
@@ -23,7 +41,9 @@ let draw (rand:System.Random) =
   let g = Graphics.FromImage i
   let f = new Font("Courier", float32 <| 24.0)
   g.FillRectangle(whiteBr, float32 <| 0.0, float32 <| 0.0, float32 <| 600.0, float32 <| 400.0 )
-  g.DrawString(text, f, redBr, float32 <| 10.0, float32 <| 40.0)
+  pal |> Array.iteri (fun i c -> g.FillRectangle((new SolidBrush(c) ), float32 <| 30*i, float32 <| 0.0, float32 <| 30*i + 30, float32 <| 64.0 ))
+
+  //g.DrawString(text, f, redBr, float32 <| 10.0, float32 <| 40.0)
   g.Flush()
   i
 
